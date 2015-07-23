fcf42ee34f223d1240f3c234300caf24cdd1f719
JanHalozan <j.halozan@gmail.com>
2015-07-23 22:46:16 +0200

    Testing YouTube playback

--- js/script.js
+++ js/script.js
@@ -1,25 +1,48 @@
+var gui = require('nw.gui');
+var win = gui.Window.get();
 var sonos = require('sonos');
 var http = require('http');
 var fs = require('fs');
+var ip = require('ip');
+var ytdl = require('ytdl-core');
+var ffmpeg = require('fluent-ffmpeg');
 
+const localIP = ip.address();
 
-const filePath = '/Users/janhalozan/Downloads/watermelonvodka.mp3';
+const filePath = '/Users/janhalozan/Downloads/Zablujena\ Generacija/komad.mp3';
 var stat = fs.statSync(filePath);
 
-http.createServer(function(request, response) {
+var server = http.createServer(function(request, response) {
     response.writeHead(200, {
-        'Content-Type': 'audio/mpeg',
-        'Content-Length': stat.size
+        'Content-Type': 'audio/mpeg'
     });
 
     // We replaced all the event handlers with a simple call to util.pump()
-    fs.createReadStream(filePath).pipe(response);
-})
-.listen(2000);
+   //  fs.createReadStream(filePath).pipe(response);
+
+   var url = 'https://www.youtube.com/watch?v=5qF_qbaWt3Q';
+   var video = ytdl(url, {filter: 'audioonly'});
+
+   new ffmpeg({source: video}).writeToStream(response, function(data, err) {
+         if (err)
+            alert(err);
+            //console.log("err");
+         });
+});
+
+server.listen(2000);
+
+win.on('close', function(){
+   this.hide();
+   server.close();
+   this.close(true);
+});
 
 $(document).ready(function() {
    devices = sonosSearch();
    setTimeout(function () {
+      $('#device-list').empty();
+
       var playableDevices = getSonosPlayableDevices(devices);
       playableDevices.forEach(function(device) {
          $('#device-list').append('<li class="item">' + device.ip + '</li>');
@@ -29,9 +52,10 @@ $(document).ready(function() {
    $(document).on('click', '#device-list .item', function(event) {
       var ip = event.target.innerHTML;
       var player = new sonos.Sonos(process.env.SONOS_HOST || ip);
-
-      player.play('http://192.168.1.117:2000/track.mp3', function (err, playing) {
-         console.log([err, playing])
+      
+      player.play('http://' + localIP + ':2000/track.mp3', function (err, playing) {
+         console.log(err);
+         console.log(playing);
       });
    });
 });
