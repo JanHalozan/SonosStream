70f04694ae8071e6b69c6e522d9d642f123bbf0c
JanHalozan <j.halozan@gmail.com>
2015-09-20 12:34:20 +0200

    Dependencies cleanup, description update

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 An app for streaming YouTube and other media to Sonos devices.
 
 In active development of a proof of concept. Stay tuned.
+
+
+#### Setup
+Currently this is a very early testing version of SonosStream. To use it one must install `ffmpeg` since there is no static library provided just yet.
+
+To install ffmpeg you can use: `brew install ffmpeg`.
+
+The app should run afterwards.
--- src/js/script.js
+++ src/js/script.js
@@ -2,25 +2,14 @@ var gui = require('nw.gui');
 var win = gui.Window.get();
 var sonos = require('sonos');
 var http = require('http');
-var fs = require('fs');
 var ip = require('ip');
 var ytdl = require('ytdl-core');
-// var ytdl = require('youtube-dl');
-// var ffmpeg = require('fluent-ffmpeg');
-// var ffmpeg = require('ffmpeg');
 
-var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
-var request = require('request');
 
 const localIP = ip.address();
 
-const filePath = '/Users/janhalozan/Downloads/Zablujena\ Generacija/komad.mp3';
-// var stat = fs.statSync(filePath);
-
 var server = http.createServer(function(req, response) {
-
-
    response.writeHead(200, {
       'Content-Type': 'audio/mpeg',
       'Content-Length': 1234556
@@ -29,7 +18,7 @@ var server = http.createServer(function(req, response) {
    var child = spawn('ffmpeg', ['-i', 'pipe:0', '-acodec', 'libmp3lame','-f', 'mp3', '-']);
    child.stdout.pipe(response);
 
-   ytdl('http://www.youtube.com/watch?v=5qF_qbaWt3Q', {filter: 'audioonly', quality: 'lowest'}).pipe(child.stdin);
+   ytdl('https://www.youtube.com/watch?v=M7QmAEN5-eQ', {filter: 'audioonly', quality: 'lowest'}).pipe(child.stdin);
 });
 
 server.listen(2000);
